ec822d5f645768f282f7bd1bf5b7fd37ccaadb91
Aswin K B <kottayilaswin14@gmail.com>
2026-09-17 00:48:03 +0530

    Packet information

--- networking/packet.py
+++ networking/packet.py
@@ -1,19 +1,33 @@
 class Packet:
 
     def __init__(self, packet_id, source, destination, data):
-        ...
+        self.packet_id = packet_id
+        self.source = source
+        self.destination = destination
+        self.data = data
+
+        self.status = "QUEUED"
+        self.timestamps = {}
 
     def update_status(self, status):
-        ...
+        self.status = status
 
     def add_timestamp(self, event, timestamp):
-        ...
+        self.timestamps[event] = timestamp
 
     def get_status(self):
-        ...
+        return self.status
 
     def get_info(self):
-        ...
+        return {
+            "packet_id": self.packet_id,
+            "source": self.source,
+            "destination": self.destination,
+            "data": self.data,
+            "status": self.status,
+            "timestamps": self.timestamps
+        }
 
-    def set_timestamp(self,event):
-        ...
+    def set_timestamp(self, event):
+        import time
+        self.timestamps[event] = time.time()
