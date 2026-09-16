1b48d0a393384dd27bae26965f11382c6a060adc
yasha351 <fathiyash7@gmail.com>
2026-09-16 20:56:29 +0530

    Implement packet loss simulation methods

--- networking/packet_loss.py
+++ networking/packet_loss.py
@@ -1,13 +1,19 @@
+import random
+
+
 class PacketLoss:
 
     def __init__(self, loss_probability):
-        ...
+        self.loss_probability = loss_probability
 
     def is_packet_lost(self):
-        ...
+        return random.random() < self.loss_probability
 
     def apply_loss(self, packet):
-        ...
+        if self.is_packet_lost():
+            packet.update_status("LOST")
+            return False
+        return True
 
     def get_loss_probability(self):
-        ...
+        return self.loss_probability
